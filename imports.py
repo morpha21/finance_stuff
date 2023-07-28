6955d875ea5b67baef56bb7d35cb484146449e97
morpha <v21.leonardo@gmail.com>
2023-07-28 14:29:50 -0300

    fixed and improved some minor stuff

---
+++ imports.py
@@ -0,0 +1,17 @@
+def imports():
+	#--------------------------------# bibliotecas
+	import requests                  #
+	import numpy        as np        #
+	import pandas        as pd       #
+	import seaborn       as sns      #
+	import yfinance       as yf      #
+	import financefunction as ff     #
+	import matplotlib.pyplot as plt  #
+	#--------------------------------#
+
+	#-------------------# prepara visualização:
+	#%matplotlib inline
+	sns.set()           #
+	#-------------------#
+
+	print("everything imported!")
