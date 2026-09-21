74deb2d9eb7979e4f880c0ac61956f715eebc9e4
morpha <morpha@debian.morpha>
2026-09-20 22:51:23 -0300

    debugando plot

--- src/finance/financefunction.py
+++ src/finance/financefunction.py
@@ -31,7 +31,7 @@ def dataplot(data, labels=['',''], size=(8,3), ylabel=None):
   axes1 = fig1.add_axes([0, 0, 1, 1])
   axes1.set_prop_cycle(cores)
   for d,l in zip(data, labels):
-    axes1.plot(data[0].index, d, label=l)
+    axes1.plot(d.index, d, label=l)
   axes1.set_ylabel(ylabel)
   axes1.legend(loc=0)
   plt.show()
